382016081aa20cae3398e15b90626a153a1cb8cc
aradmand <ar4420@gmail.com>
2014-11-09 11:23:13 -0800

    Wrap entire algo Trade method in begin..rescue

--- bin/algo.rb
+++ bin/algo.rb
@@ -18,64 +18,66 @@ def set_trading_parameters
 end
 
 def trade(buy_exchange, sell_exchange)
-  percent = MIN_PERCENT_PROFIT
-  sleep(2.0)
-  puts
-  puts
-  puts
+  begin
+    percent = MIN_PERCENT_PROFIT
+    sleep(2.0)
+    puts
+    puts
+    puts
 
-  start_time = Time.now
+    start_time = Time.now
 
-  puts "#=================="
-  puts "[Timestamp - #{start_time}]"
+    puts "#=================="
+    puts "[Timestamp - #{start_time}]"
 
-  options = { buyer: buy_exchange, seller: sell_exchange, volume: @volume, cutoff: percent, verbose: true}
-  rbtc_arbitrage = RbtcArbitrage::Trader.new(options)
+    options = { buyer: buy_exchange, seller: sell_exchange, volume: @volume, cutoff: percent, verbose: true}
+    rbtc_arbitrage = RbtcArbitrage::Trader.new(options)
 
-  command = "rbtc --seller #{sell_exchange} --buyer #{buy_exchange} --volume #{@volume} --cutoff #{percent}"
+    command = "rbtc --seller #{sell_exchange} --buyer #{buy_exchange} --volume #{@volume} --cutoff #{percent}"
 
-  puts "rbtc_arbitrage command:"
-  puts command
+    puts "rbtc_arbitrage command:"
+    puts command
 
-  btc_balance, usd_balance = rbtc_arbitrage.get_buyer_balance
-  puts "[ Balance on buyer (#{buy_exchange}) exchange: (#{usd_balance} in USD) (#{btc_balance} in BTC) ]"
+    btc_balance, usd_balance = rbtc_arbitrage.get_buyer_balance
+    puts "[ Balance on buyer (#{buy_exchange}) exchange: (#{usd_balance} in USD) (#{btc_balance} in BTC) ]"
 
-  btc_balance, usd_balance = rbtc_arbitrage.get_seller_balance
-  puts "[ Balance on seller (#{sell_exchange}) exchange: (#{usd_balance} in USD) (#{btc_balance} in BTC) ]"
+    btc_balance, usd_balance = rbtc_arbitrage.get_seller_balance
+    puts "[ Balance on seller (#{sell_exchange}) exchange: (#{usd_balance} in USD) (#{btc_balance} in BTC) ]"
 
-  buyer_price = rbtc_arbitrage.fetch_buyer_price
-  puts "[ Ask on buyer (#{buy_exchange}) exchange: (#{buyer_price} in dollars) ]"
+    buyer_price = rbtc_arbitrage.fetch_buyer_price
+    puts "[ Ask on buyer (#{buy_exchange}) exchange: (#{buyer_price} in dollars) ]"
 
-  seller_price = rbtc_arbitrage.fetch_seller_price
-  puts "[ Bid on sell (#{sell_exchange}) exchange: (#{seller_price} in dollars) ]"
+    seller_price = rbtc_arbitrage.fetch_seller_price
+    puts "[ Bid on sell (#{sell_exchange}) exchange: (#{seller_price} in dollars) ]"
 
-  profit_dollars, profit_percent = rbtc_arbitrage.get_profit
-  puts "Profit: $#{profit_dollars} --> #{profit_percent}%"
+    profit_dollars, profit_percent = rbtc_arbitrage.get_profit
+    puts "Profit: $#{profit_dollars} --> #{profit_percent}%"
 
-  if profit_percent > percent
+    if profit_percent > percent
     puts "PROFITABLE TRADE!"
     @accumulated_profit_in_cents += (profit_dollars * 100)
-  end
-  puts "ACCUMULATED PROFIT: #{@accumulated_profit_in_cents / 100.0}"
+    end
+    puts "ACCUMULATED PROFIT: #{@accumulated_profit_in_cents / 100.0}"
 
-  end_time = Time.now
-  puts "[Timestamp - #{end_time}]"
-  puts "[Elapsed time - #{end_time - start_time}]"
-  puts
+    end_time = Time.now
+    puts "[Timestamp - #{end_time}]"
+    puts "[Elapsed time - #{end_time - start_time}]"
+    puts
 
-  begin
-      puts "\t---Executing command---"
-      start_time = Time.now
-      rbtc_arbitrage.trade
-      end_time = Time.now
-      puts "[Elapsed time - #{end_time - start_time}]"
-      puts "\t---Done excecuting command---"
-      puts "#=================="
+    puts "\t---Executing command---"
+    start_time = Time.now
+    rbtc_arbitrage.trade
+    end_time = Time.now
+    puts "[Elapsed time - #{end_time - start_time}]"
+    puts "\t---Done excecuting command---"
+    puts "#=================="
+
   rescue Exception => e
     puts " *** Exception has occured *** "
     puts e.message
     puts "************"
   end
+
   profit_dollars
 end
 
