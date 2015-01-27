df92276c16f69a19a1cc45dc1e5b1d5716e9c1d8
Arian <ar4420@gmail.com>
2015-01-27 01:37:42 -0500

    Implement price method

---
+++ bin/coinbase_exchange_client_tests.rb
@@ -0,0 +1,46 @@
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'rbtc_arbitrage'
+require 'pry'
+require 'curb'
+require 'active_support'
+require 'json'
+
+
+# Instantiate Coinbase Exchange Client
+coinbase_exchange_client = RbtcArbitrage::Clients::CoinbaseExchangeClient.new
+
+# Instatiate Coinbase Client (note, different from CoinbaseExchangeClient)
+coinbase_client = RbtcArbitrage::Clients::CoinbaseClient.new
+
+
+####################
+# Price
+####################
+
+coinbase_buy_price = coinbase_client.price(:buy)
+coinbase_sell_price = coinbase_client.price(:sell)
+
+puts 'coinbase_buy_price'
+puts coinbase_buy_price
+
+puts 'coinbase_sell_price'
+puts coinbase_sell_price
+
+coinbase_exchange_buy_price = coinbase_exchange_client.price(:buy)
+coinbase_exchange_sell_price = coinbase_exchange_client.price(:sell)
+
+puts 'coinbase_exchange_buy_price'
+puts coinbase_exchange_buy_price
+
+puts 'coinbase_exchange_sell_price'
+puts coinbase_exchange_sell_price
+
+
+
+
+
+
+
+
+
+
--- lib/rbtc_arbitrage/clients/coinbase_exchange_client.rb
+++ lib/rbtc_arbitrage/clients/coinbase_exchange_client.rb
@@ -3,6 +3,11 @@ module RbtcArbitrage
     class CoinbaseExchangeClient
       include RbtcArbitrage::Client
 
+      require 'pry'
+      require 'curb'
+      require 'active_support'
+      require 'json'
+
       # return a symbol as the name
       # of this exchange
       def exchange
@@ -29,6 +34,7 @@ module RbtcArbitrage
       # `action` is :buy or :sell
       # Returns a Numeric type.
       def price action
+        order_book_command(action)
       end
 
       # Transfers BTC to the address of a different
@@ -41,6 +47,74 @@ module RbtcArbitrage
       # remove this method and set the ENV
       # variable [this-exchange-name-in-caps]_ADDRESS
       def address
+      end
+
+      private
+
+      def exchange_api_url
+        'https://api.exchange.coinbase.com'
+      end
+
+      def order_book_command(action)
+        product_id = products_command
+        api_url = "#{exchange_api_url}/products/#{product_id}/book?level=2"
+
+        path_header = "/products/#{product_id}/book?level=2"
+
+        curl = Curl::Easy.new(api_url) do |http|
+          http.headers['host'] = 'api.exchange.coinbase.com'
+          http.headers['method'] = 'GET'
+          http.headers['path'] = path_header
+          http.headers['scheme'] = 'https'
+          http.headers['version'] = 'HTTP/1.1'
+          http.headers['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+          http.headers['accept-encoding'] = 'gzip,deflate,sdch'
+          http.headers['accept-language'] = 'en-US,en;q=0.8'
+          http.headers['user-agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+        end
+
+        response = curl.perform
+
+        json_data = ActiveSupport::Gzip.decompress(curl.body_str)
+        parsed_json = JSON.parse(json_data)
+
+        if action == :buy
+          #buy = asks
+          price_entry = parsed_json['asks'].third
+          price_entry.first.try(:to_f)
+        else
+          #sell = bids
+          price_entry = parsed_json['bids'].third
+          price_entry.first.try(:to_f)
+        end
+      end
+
+      def products_command
+        api_url = "#{exchange_api_url}/products"
+        path_header = '/products'
+
+        curl = Curl::Easy.new(api_url) do |http|
+          http.headers['host'] = 'api.exchange.coinbase.com'
+          http.headers['method'] = 'GET'
+          http.headers['path'] = path_header
+          http.headers['scheme'] = 'https'
+          http.headers['version'] = 'HTTP/1.1'
+          http.headers['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+          http.headers['accept-encoding'] = 'gzip,deflate,sdch'
+          http.headers['accept-language'] = 'en-US,en;q=0.8'
+          http.headers['user-agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+        end
+
+        response = curl.perform
+
+        json_data = ActiveSupport::Gzip.decompress(curl.body_str)
+        parsed_json = JSON.parse(json_data)
+
+        product_id = parsed_json.map do |product|
+          if product['id'] == 'BTC-USD'
+            product['id']
+          end
+        end.first
       end
     end
   end
