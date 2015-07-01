2dd925479e2c7c4b013a7e85683b3304e9ad0712
Arian <ar4420@gmail.com>
2015-06-30 20:03:39 -0400

    Create circle position initializer

---
+++ lib/circle_position_initializer.rb
@@ -0,0 +1,15 @@
+module CirclePositionInitializer
+
+  # The CirclePositionInitializer is designed to initialize positions in all configured
+  # circle accounts.
+
+  # For each configured account, the CirclePositionInitializer will:
+  # 1. Determine the account balance in BTC
+  # 2. If the balance is less than the desired amount, CirclePositionInitializer will
+  #   - Fill the account with BTC up to the desired amount
+
+  class CirclePositionInitializer
+
+
+  end
+end
